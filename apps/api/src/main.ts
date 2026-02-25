4ace4512b2ad093a6f9bdf441c65b4461f1bef34
armando3069 <armandoburuian1234@gmail.com>
2026-02-25 05:54:17 +0200

    feat: add Cors to main app

---
+++ apps/api/src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,       // strip unknown properties from DTOs
+      forbidNonWhitelisted: true,
+      transform: true,       // auto-transform payloads to DTO class instances
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // dacă Next e pe 3000
+      'http://localhost:3001', // dacă Next e pe 3001
+    ],
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  await app.listen(3001);
+}
+
+bootstrap();
