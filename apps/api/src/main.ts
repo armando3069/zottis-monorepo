8511c1f3dd883ef09fe45f606908e6d22ce49dfd
armando3069 <armandoburuian1234@gmail.com>
2026-02-28 07:00:33 +0200

    refactor: add logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['log', 'debug', 'warn', 'error'] });
 
   app.useWebSocketAdapter(new IoAdapter(app));
 
